bc2ebbe3c50dd7f44dceef22e1ed55bd46b744fb
Avishek <avishekprasad12345@gmail.com>
2026-02-03 17:52:18 +0530

    Ques on Exception Propagation

---
+++ Exception3rdFeb/ExceptionPropagation.cs
@@ -0,0 +1,44 @@
+
+class Controller
+{
+    static void Main()
+    {
+        // TODO:
+        // Call Service method
+        // Handle exception here
+        try
+        {
+            Service.Process();
+        }catch(Exception ex)
+        {
+            System.Console.WriteLine("Now in Controller "+ex.Message);
+        }
+    }
+}
+
+class Service
+{
+    public static void Process()
+    {
+        // TODO:
+        // Call Repository method
+        // Catch, log and rethrow exception
+        try
+        {
+            Repository.GetData();
+
+        }catch(Exception ex)
+        {
+            System.Console.WriteLine("Logging Exception from called method "+ex.Message);
+            Repository.GetData();
+        }
+    }
+}
+
+class Repository
+{
+    public static void GetData()
+    {
+        throw new FormatException("Exception in Repo");
+    }
+}
